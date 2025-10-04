e153a1e4b72e71f8309c4df7d8738b16fc3f3ae7
nikgrinding <nikgrinding@gmail.com>
2025-10-05 05:21:17 +0530

    ui tweaks

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 load_dotenv()
 
@@ -31,6 +32,8 @@ migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 
 mail = Mail(app)
+
+csrf = CSRFProtect(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
